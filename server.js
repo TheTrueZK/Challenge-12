ba7d0a8c00be8b6a86de1826008e8ec0dca5ce56
TheTrueZK <thetruezk@gmail.com>
2022-08-17 11:19:34 -0500

    added choices to prompt

--- server.js
+++ server.js
@@ -16,4 +16,35 @@ function initialPrompt() {
                 "Add Department"
             ]
         })
+        .then(function ({task}) {
+            switch (task) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+
+                case "Add Department":
+                    addEmployee();
+                    break;
+            }
+        });
 }
